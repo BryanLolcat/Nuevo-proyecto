a14d4f294aa6d41edcc522b72e172afb59d08f96
BryanLolcat <54367689+BryanLolcat@users.noreply.github.com>
2019-09-27 11:20:38 -0500

    Update main.cpp

--- arreglos/main.cpp
+++ arreglos/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 void Imprimir(int n[],int tam){
     int i = 0;
     while (i < tam){
-        cout << n[i] << endl;
+        cout << n[i];
         i++;
     }
 }
@@ -19,16 +19,17 @@ int SumaIter(int n[],int tam){
     return suma ;
 }
 
-/*int SumaRec(int n[],int tam){
-    int i = 0;
+int SumaRec(int n[],int tam){
     if (tam == 1)
-        return n[i];
-    if (tam != 1)
-        return (n[(tam-1)] + SumaRec(n[],(tam-1));
-}*/
+        return n[0];
+    else
+        return n[tam-1] + SumaIter(n,tam-1);
+}
+
 
 void RevertIter(int n[],int tam){
     int i = 0;
+    tam--;
     while (i < (tam/2)){
         int c;
         c = n[i];
@@ -38,11 +39,26 @@ void RevertIter(int n[],int tam){
     }
 
 }
+ void RevertRec(int n[],int tam){
+    int i = 0;
+    tam--;
+    if (tam > i)
+        int c;
+        int c;
+        c = n[i];
+        n[i] = n[tam-i];
+        n[tam-i] = c;
+        i++;
+        return RevertRec(n,tam);
 
+ }
 
 int main()
 {
     int arr[]={1,2,3,4,5};
     Imprimir(arr,5);
-    cout << SumaIter(arr,5);
+    cout << SumaIter(arr,5) << endl;
+    cout << SumaRec(arr,5) << endl;
+    RevertRec(arr,5);
+    Imprimir(arr,5);
 }
